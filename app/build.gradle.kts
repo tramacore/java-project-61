fecfae5b2058e66a0b422c28f01093b2a0994f4f
LobotomyKuro <nojji@yandex.ru>
2024-02-25 16:40:08 +0300

    second task

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    application
 }
 
 group = "hexlet.code"
@@ -17,4 +18,8 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+application {
+    mainClass.set ("hexlet.code.App")
 }
