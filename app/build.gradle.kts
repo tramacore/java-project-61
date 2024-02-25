db0fc2ee923d9481c0c6ec9282bdf6bba20c2559
LobotomyKuro <nojji@yandex.ru>
2024-02-25 15:31:07 +0300

    added main package

---
+++ app/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation ("org.apache.commons:commons-lang3:3.14.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
