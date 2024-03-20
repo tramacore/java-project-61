6584b7d1de01b00331046b52bc70c6d72367ea0e
tramacore <138532257+tramacore@users.noreply.github.com>
2024-03-20 19:57:13 +0300

    Update Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -3,6 +3,7 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 
 public class Even {
+    private static final int EVENMETHOD = 2;
     private static int quest;
     private static final int FACTOR = 100; //т.к Math.random() генерирует число от 0 до 1 - то умножаем это число на 100
     private static final int COUNTROUNDS = 3; //Кол-во раундов - 3
@@ -27,6 +28,6 @@ public class Even {
         return arrayQuests;
     }
     public static void play() {
-        Engine.starter(2 ,even() ,getQuest());
+        Engine.starter(EVENMETHOD, even(), getQuest());
     }
 }
